50269da3436e3f52b9f80ae3cfc722eaa80027ad
FelicitasLan <forsubscriptionsite@gmail.com>
2022-03-05 16:43:25 +0100

    first commit

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+}
+
+
+module.exports = {
+  images: {
+    domains: ["www.edigitalagency.com.au", "www.pngfind.com", "upload.wikimedia.org", "scontent.fzrh1-1.fna.fbcdn.net"]
+  }
+}
